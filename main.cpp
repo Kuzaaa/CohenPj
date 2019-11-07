923a77cec966f9e3ef0a0976915a07b6da172987
RKettemeyer <r.kettemeyer@gmail.com>
2019-11-07 15:34:18 +0100

    update BronKerbosch

--- main.cpp
+++ main.cpp
@@ -5,11 +5,29 @@
 using namespace std;
 
 int main(){
-	Graph graphe(6);
+	vector<int> P;
+	vector<int> R;
+	vector<int> X;
+
+	Graph graphe(5);
 	srand(time(NULL));
-	graphe.generation_aleatoire2();
+	graphe.generation_aleatoire1();
+
+	for(int i=0; i<=graphe.nb_sommet; i++){
+		P.push_back(i);
+	}
+
 	graphe.affiche();
-	graphe.degeneracy();
+	graphe.BronKerbosch(P, R, X);
+
+	cout << "List clique maximale : ";
+	for(auto clique : graphe.clique_maximal){
+		cout << "[ ";
+		for(auto sommet : clique){
+			cout << sommet << ", ";
+		}
+		cout << "]" << endl;
+	}
 
 	return 0;
 }
