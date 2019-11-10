ac408f1b2a27fbca9eec6a5bf36ae088db154098
RKettemeyer <r.kettemeyer@gmail.com>
2019-11-10 01:37:54 +0100

    Un zouli main: whaaaaaaaaaaw *-*

--- main.cpp
+++ main.cpp
@@ -5,31 +5,67 @@
 
 using namespace std;
 
+//recupère une entrée de type int
+void entree(int &choix)
+{
+	cin >> choix;
+	if(cin.fail())
+	{
+		cout << "Saisie incorrecte." << endl;
+		cin.clear();
+	}
+	cin.ignore(numeric_limits<streamsize>::max(),'\n');
+}
+
+
 int main(){
 
+	int nb_sommet,choix;
+	vector<int> P,R,X;
+	
+	do{
+		cout << "Entrer le nombre de sommets ( >= 1 ): ";
+		entree(nb_sommet);
+	}while(nb_sommet < 1);
 
-	vector<int> P;
-	vector<int> R;
-	vector<int> X;
+	Graph graphe(nb_sommet);
 
-	Graph graphe(8);
-	srand(time(NULL));
-	graphe.generation_aleatoire2();
-	//graphe.maximal_clique_enumeration1();
-	graphe.maximal_clique_enumeration2();
+	do{
+		cout << "Comment voulez-vous générer votre graphe ?:\n(1)Aléatoirement\n(2)Avec l'algorithme de Barabèsi-Albert" << endl;
+		entree(choix);
+	}while((choix < 1)||(choix>2));
 
-	/*for(int i=0; i<graphe.nb_sommet; i++){
-		P.push_back(i);
+	if(choix==1)
+		graphe.generation_aleatoire1();
+	else
+		graphe.generation_aleatoire2();
+
+	do{
+		cout << "Que voulez-vous faire ?:\n(1)Bron-Kerbosch\n(2)Bron-Kerbosch avec pivot"
+			"\n(3)Enumeration de cliques maximales avec arbre de suffixe"
+			"\n(4)Enumeration de cliques maximales sans arbre de suffixe" << endl;
+		entree(choix);
+	}while((choix < 1)||(choix>4));
+
+	switch (choix) {
+		case 1:
+			for(int i=0; i<nb_sommet; i++){
+				P.push_back(i);
+			}
+			graphe.BronKerbosch(&P,&R,&X);
+			break;
+		case 2:
+			cout << "The cake is a lie" << endl;
+			break;
+		case 3:
+			graphe.maximal_clique_enumeration1();
+			break;
+		case 4:
+			graphe.maximal_clique_enumeration2();
+			break;
+		default:
+			cout << "You shouldn't be here..." << endl;
 	}
-	graphe.affiche();
-
-	cout << "Graphe2: avec sommet 3:" << endl;
-	Graph graphe2(3,graphe);
-	graphe2.affiche();*/
-
-	//graphe.degeneracy();
-	//graphe.liste_adj_degen();
-	//graphe.bron_kerbosch_degeneracy();
 
 	return 0;
 }
