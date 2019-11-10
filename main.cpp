c19c94f040c4ead8920a85ef1aae51298f782acc
RKettemeyer <r.kettemeyer@gmail.com>
2019-11-10 02:02:53 +0100

    Hop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+#include <limits>
+#include <string>
+#include <chrono>
 #include "Graph.hpp"
 #include "suffix_hash_map.hpp"
 
@@ -22,49 +24,68 @@ int main(){
 
 	int nb_sommet,choix;
 	vector<int> P,R,X;
-	
-	do{
-		cout << "Entrer le nombre de sommets ( >= 1 ): ";
-		entree(nb_sommet);
-	}while(nb_sommet < 1);
+	std::chrono::system_clock::time_point start;
+	std::chrono::duration<double> sec;
 
-	Graph graphe(nb_sommet);
+	while(1){
+		do{
+			cout << "Entrer le nombre de sommets ( >= 1 ): ";
+			entree(nb_sommet);
+		}while(nb_sommet < 1);
 
-	do{
-		cout << "Comment voulez-vous générer votre graphe ?:\n(1)Aléatoirement\n(2)Avec l'algorithme de Barabèsi-Albert" << endl;
-		entree(choix);
-	}while((choix < 1)||(choix>2));
+		Graph graphe(nb_sommet);
 
-	if(choix==1)
-		graphe.generation_aleatoire1();
-	else
-		graphe.generation_aleatoire2();
+		do{
+			cout << "Comment voulez-vous générer votre graphe ?:\n(1)Aléatoirement\n(2)Avec l'algorithme de Barabèsi-Albert" << endl;
+			entree(choix);
+		}while((choix < 1)||(choix>2));
 
-	do{
-		cout << "Que voulez-vous faire ?:\n(1)Bron-Kerbosch\n(2)Bron-Kerbosch avec pivot"
-			"\n(3)Enumeration de cliques maximales avec arbre de suffixe"
-			"\n(4)Enumeration de cliques maximales sans arbre de suffixe" << endl;
-		entree(choix);
-	}while((choix < 1)||(choix>4));
+		if(choix==1)
+			graphe.generation_aleatoire1();
+		else
+			graphe.generation_aleatoire2();
 
-	switch (choix) {
-		case 1:
-			for(int i=0; i<nb_sommet; i++){
-				P.push_back(i);
-			}
-			graphe.BronKerbosch(&P,&R,&X);
-			break;
-		case 2:
-			cout << "The cake is a lie" << endl;
-			break;
-		case 3:
-			graphe.maximal_clique_enumeration1();
-			break;
-		case 4:
-			graphe.maximal_clique_enumeration2();
-			break;
-		default:
-			cout << "You shouldn't be here..." << endl;
+		do{
+			cout << "Que voulez-vous faire ?:\n(1)Bron-Kerbosch\n(2)Bron-Kerbosch avec pivot"
+				"\n(3)Enumeration de cliques maximales avec arbre de suffixe"
+				"\n(4)Enumeration de cliques maximales sans arbre de suffixe" << endl;
+			entree(choix);
+		}while((choix < 1)||(choix>4));
+
+		switch (choix) {
+			case 1:
+				for(int i=0; i<nb_sommet; i++){
+					P.push_back(i);
+				}
+				start = std::chrono::system_clock::now();
+				graphe.BronKerbosch(&P,&R,&X);
+				sec = std::chrono::system_clock::now() - start;
+				break;
+			case 2:
+				cout << "The cake is a lie" << endl;
+				break;
+			case 3:
+				start = std::chrono::system_clock::now();
+				graphe.maximal_clique_enumeration1();
+				sec = std::chrono::system_clock::now() - start;
+				break;
+			case 4:
+				start = std::chrono::system_clock::now();
+				graphe.maximal_clique_enumeration2();
+				sec = std::chrono::system_clock::now() - start;
+				break;
+			default:
+				cout << "You shouldn't be here..." << endl;
+		}
+
+		do{
+			cout << "Voulez-vous recommencer ?:\n(1)Oui\n(2)Quitter" << endl;
+			entree(choix);
+		}while((choix < 1)||(choix>2));
+
+		if(choix==2)
+			return 0;
+
 	}
 
 	return 0;
